db24ed877c2be49f29d43fe61027d93230248c83
0PAMP <jsm040514@gmail.com>
2026-02-15 23:50:40 +0900

    Test

---
+++ Test/Test.srcs/sources_1/new/top.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/02/15 20:58:28
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(output [3:0] led );
+    assign led = 4'b1010;
+endmodule
